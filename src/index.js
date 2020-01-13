2029b225924b02e5601f96ab2b3016b74cab0f4d
Jess <jessbennett924@gmail.com>
2020-01-12 21:39:11 -0500

    extracting userId from header

--- src/index.js
+++ src/index.js
@@ -4,23 +4,30 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-
-// ApolloClient setup
-
-// dependencies
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
 
-// httpLink that will connect the ApolloClient
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 // instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
